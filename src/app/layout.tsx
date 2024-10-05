a393d1f20310b0c95fd90ce8e04c7ff86aae1efa
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-10-05 15:13:50 -0400

    fix: geist font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,8 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { GeistSans } from "geist/font/sans";
+// import { GeistMono } from "geist/font/mono";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
-
-const geistSans = localFont({
-	src: "./fonts/GeistVF.woff",
-	variable: "--font-geist-sans",
-	weight: "100 900",
-});
-const geistMono = localFont({
-	src: "./fonts/GeistMonoVF.woff",
-	variable: "--font-geist-mono",
-	weight: "100 900",
-});
 
 export const metadata: Metadata = {
 	title: "Create Next App",
@@ -26,9 +16,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body
-				className={`${geistSans.variable}${geistMono.variable} antialiased`}
-			>
+			<body className={`${GeistSans.className} antialiased`}>
 				<ThemeProvider
 					attribute="class"
 					defaultTheme="system"
