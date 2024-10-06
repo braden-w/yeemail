da1b1a63fe4cfa4ea8f4ef041faae0d48fdd8028
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-10-06 10:58:53 -0400

    feat: redirect to launch

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,8 +1,10 @@
 import SignOutBtn from "@/components/auth/SignOutBtn";
 import { getUserAuth } from "@/lib/auth/utils";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
 	const { session } = await getUserAuth();
+	redirect("/launch");
 	return (
 		<main className="">
 			<h1 className="my-2 font-bold text-2xl">Profile</h1>
