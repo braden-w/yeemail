93b99d2019ba7262b9fa49dcfbde195a4bb02dc1
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-10-05 21:12:01 -0400

    feat: yeemail topbar

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,9 +1,11 @@
+import { Logo } from "@/components/Logo";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
 import { Toaster } from "@/components/ui/sonner";
 import { checkAuth } from "@/lib/auth/utils";
 import TrpcProvider from "@/lib/trpc/Provider";
 import { cookies } from "next/headers";
+import Link from "next/link";
 export default async function AppLayout({
 	children,
 }: {
@@ -13,12 +15,31 @@ export default async function AppLayout({
 	return (
 		<main>
 			<TrpcProvider cookies={cookies().toString()}>
-				<div className="flex h-screen">
-					<Sidebar />
-					<main className="flex-1 overflow-y-auto p-8 pt-2 md:p-8">
-						<Navbar />
-						{children}
-					</main>
+				<div className="flex h-screen flex-col">
+					<header className="sticky top-0 z-50 flex min-h-14 w-full items-center border-border/40 bg-background/95 px-4 backdrop-blur supports-[backdrop-filter]:bg-background/60 lg:px-6">
+						<Logo />
+						<nav className="ml-auto flex gap-4 sm:gap-6">
+							<Link
+								className="font-medium text-sm underline-offset-4 hover:underline"
+								href="#features"
+							>
+								Features
+							</Link>
+							<Link
+								className="font-medium text-sm underline-offset-4 hover:underline"
+								href="/sign-in"
+							>
+								Sign In
+							</Link>
+						</nav>
+					</header>
+					<div className="flex flex-1">
+						<Sidebar />
+						<main className="flex-1 overflow-y-auto p-8 pt-2 md:p-8">
+							<Navbar />
+							{children}
+						</main>
+					</div>
 				</div>
 			</TrpcProvider>
 
