158b6bde4f277ef4d815974c374c970e627d715b
Braden Wong <13159333+braden-w@users.noreply.github.com>
2024-10-06 10:27:10 -0400

    feat: useMutation and useRouter on launch page

--- src/lib/server/routers/_app.ts
+++ src/lib/server/routers/_app.ts
@@ -1,5 +1,6 @@
 import { router } from "@/lib/server/trpc";
 import { emailsRouter } from "./emails";
+import { launchRouter } from "./launch";
 import { savedEventsRouter } from "./savedEvents";
 import { schedulesRouter } from "./schedules";
 import { suggestedEventsRouter } from "./suggestedEvents";
@@ -9,6 +10,7 @@ export const appRouter = router({
 	savedEvents: savedEventsRouter,
 	schedules: schedulesRouter,
 	emails: emailsRouter,
+	launch: launchRouter,
 });
 
 export type AppRouter = typeof appRouter;
